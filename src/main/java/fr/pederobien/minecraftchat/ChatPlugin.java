c8985803eab6a3037c7c4591a618970c21900bf8
Pierre-Emmanuel41 <pierre.emmanuel41@gmail.com>
2020-08-15 19:31:49 +0200

    Updating file pom.xml, adding file plugin.yml and class ChatPlugin

---
+++ src/main/java/fr/pederobien/minecraftchat/ChatPlugin.java
@@ -0,0 +1,16 @@
+package fr.pederobien.minecraftchat;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ChatPlugin extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		super.onEnable();
+	}
+
+	@Override
+	public void onDisable() {
+		super.onDisable();
+	}
+}
