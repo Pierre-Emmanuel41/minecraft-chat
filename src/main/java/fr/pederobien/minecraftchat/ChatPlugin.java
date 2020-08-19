05ef6ccb79e6afe2addbdaa218bc21326779ace6
Pierre-Emmanuel41 <pierre.emmanuel41@gmail.com>
2020-08-19 22:14:14 +0200

    Preparing adding command chatconfig

--- src/main/java/fr/pederobien/minecraftchat/ChatPlugin.java
+++ src/main/java/fr/pederobien/minecraftchat/ChatPlugin.java
@@ -1,16 +1,47 @@
 package fr.pederobien.minecraftchat;
 
+import java.io.FileNotFoundException;
+import java.nio.file.Path;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
+import fr.pederobien.dictionary.interfaces.IDictionaryParser;
+import fr.pederobien.minecraftgameplateform.utils.Plateform;
+
 public class ChatPlugin extends JavaPlugin {
+	public static final String NAME = "minecraft-chat";
 
 	@Override
 	public void onEnable() {
-		super.onEnable();
+		Plateform.getPluginHelper().register(this);
+		registerDictionaries();
 	}
 
 	@Override
 	public void onDisable() {
 		super.onDisable();
+	}
+
+	private void registerDictionaries() {
+		// Registering French dictionaries
+		registerDictionary("French", "Chat.xml");
+
+		// Registering English dictionaries
+		registerDictionary("English", "Chat.xml");
+	}
+
+	private void registerDictionary(String parent, String... dictionaryNames) {
+		Path jarPath = Plateform.ROOT.getParent().resolve(NAME.concat(".jar"));
+		String dictionariesFolder = "resources/dictionaries/".concat(parent).concat("/");
+		for (String name : dictionaryNames)
+			registerDictionary(Plateform.getDefaultDictionaryParser(dictionariesFolder.concat(name)), jarPath);
+	}
+
+	private void registerDictionary(IDictionaryParser parser, Path jarPath) {
+		try {
+			Plateform.getNotificationCenter().getDictionaryContext().register(parser, jarPath);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
 }
